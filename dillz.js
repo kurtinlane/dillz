db43bcbdc2810b95df76dd79c460873d6e317514
Colton Shields <colton@bettrnet.com>
2015-10-02 08:01:20 -0700

    button sends text!

--- dillz.js
+++ dillz.js
@@ -11,6 +11,33 @@ if (Meteor.isServer) {
       ]
     });
   });
+
+	Meteor.methods({
+    'sendText': function(){
+        console.log("SERVER send text");
+				var ACCOUNT_SID = 'ACcd11ee8452c1eacb8a16599b9a3662da';
+				var AUTH_TOKEN = 'bb6b4d096ed678f6263cbdcabd535094';
+				var ben = '+18018427745';
+				var colton = '+18019716820'
+
+				twilio = Twilio(ACCOUNT_SID, AUTH_TOKEN);
+					twilio.sendSms({
+						to: colton, // Any number Twilio can deliver to
+						from: '+18019809306', // A number you bought from Twilio and can use for outbound communication
+						body: 'Come to dinner at 6 plz.' // body of the SMS message
+					}, function(err, responseData) { //this function is executed when a response is received from Twilio
+						if (err) {
+							console.log('ERROR SENDING TEXT',err);
+							return err;
+						}
+							console.log('to: ', responseData.to); // outputs "+14506667788"
+							console.log('from: ', responseData.from); // outputs "+14506667788"
+							console.log(responseData.statusCode); // outputs "+14506667788"
+							console.log('body: ', responseData.body); // outputs "word to your mother."
+							return responseData;
+					});
+    }
+	});
 }
 
 if (Meteor.isClient) {
@@ -49,6 +76,12 @@ if (Meteor.isClient) {
       // Clear form
       event.target.text.value = "";
     },
+		"click .send-text": function (event) {
+			console.log("send text");
+	    Meteor.call("sendText", function (response) {
+	    	console.log(response);
+	    });
+		},
     "change .hide-completed input": function (event) {
       Session.set("hideCompleted", event.target.checked);
     }
